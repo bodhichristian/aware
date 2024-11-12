28bce9c25cc75c6f160add1d3419a3b621b56230
bodhichristian <clavelle23@gmail.com>
2024-11-12 07:15:48 -0500

    add unit tests for array extension

---
+++ Aware/ArrayExtensionTests.swift
@@ -0,0 +1,24 @@
+//
+//  ArrayExtensionTests.swift
+//  AwareTests
+//
+//  Created by christian on 11/12/24.
+//
+
+import Testing
+@testable import Aware
+
+struct ArrayExtensionTests {
+    @Test func arrayAverageForInt() {
+        let arr = [1, 2, 3, 4, 5]
+        let expectedAverage = 3
+        let average = arr.average
+        #expect(average == expectedAverage)
+    }
+    
+    @Test func arrayAverageReturnsZeroWhenEmpty() {
+        let arr: [Int] = []
+        let average = arr.average
+        #expect(average == 0)
+    }
+}
